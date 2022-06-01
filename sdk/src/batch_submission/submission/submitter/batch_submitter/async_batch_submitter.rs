d91aceb60abf15c00a65f8c0713a582824388644
Chris Eckhardt <eckhardt@bitwise.io>
2022-06-01 14:12:45 -0500

    Clean up use semantics
    Signed-off-by: Chris Eckhardt <eckhardt@bitwise.io>

--- sdk/src/batch_submission/submission/submitter/batch_submitter/async_batch_submitter.rs
+++ sdk/src/batch_submission/submission/submitter/batch_submitter/async_batch_submitter.rs
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::{fmt, sync::Arc};
+
 use async_trait::async_trait;
 use reqwest::Client;
-
-use std::{fmt, sync::Arc};
 
 use crate::{
     batch_submission::{
